f3ed5565a2a2a1e19fc239b36de97f7c1e14c08e
Suh Moraes <suh.alvesmoraes@gmai.com>
2021-01-29 19:54:30 -0300

    Instalando as dependências

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const { Router, response } = require('express');
+
+const routes = Router();
+
+routes.get('/', (request, response) => {
+  return response.json({ sucesso: 'Você chegou até aqui!' });
+});
+
+module.exports = routes;
